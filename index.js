4e1d52c81772d805ed1d9147573377755735671b
demosjarco <demosjarco@users.noreply.github.com>
2020-02-07 11:51:42 -0700

    Create separate function for A and AAAA

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ request({
 		if (body.success) {
 			body.result.forEach(function (dnsRecord) {
 				if (dnsRecord.name == process.env.CLOUDFLARE_UPDATEFOR) {
-					if (dnsRecord.type == 'A' || dnsRecord.type == 'AAAA') {
-						updateCloudflare(dnsRecord);
+					if (dnsRecord.type == 'A') {
+						updateCloudflareA(dnsRecord);
+					} else if (dnsRecord.type == 'AAAA') {
+						updateCloudflareAAAA(dnsRecord);
 					}
 				}
 			});
@@ -29,56 +31,90 @@ request({
 	}
 });
 
-function updateCloudflare(dnsRecord) {
+function updateCloudflareA(dnsRecord) {
 	if (dnsRecord) {
-		console.log(dnsRecord.type);
-		let getIpUrl = '';
-		if (dnsRecord.type == 'A') {
-			getIpUrl = 'https://api.ipify.org/?format=json';
-		} else if (dnsRecord.type == 'AAAA') {
-			getIpUrl = 'https://api6.ipify.org/?format=json';
-		}
-		console.log(getIpUrl);
-		if (getIpUrl != '') {
-			request.get({
-				url: getIpUrl,
-				json: true
-			}, function (error1, response1, body1) {
-				if (error1) {
-					throw error1
-				} else if (response1.statusCode != 200) {
-					throw new Error('ip address request http ' + response1.statusCode);
-				} else {
-					console.log(body1);
-					const validIp = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/;
-					if (validIp.test(body1.ip)) {
-						const currentIp = validIp.exec(body1.ip)[0];
-						console.log(currentIp);
-						console.log(dnsRecord.type);
-						request.put({
-							url: 'https://api.cloudflare.com/client/v4/zones/' + process.env.CLOUDFLARE_ZONEIDENTIFIER + '/dns_records/' + dnsRecord.id,
-							headers: {
-								'Content-Type': 'application/json',
-								Authorization: 'Bearer ' + process.env.CLOUDFLARE_APIKEY
-							},
-							body: JSON.stringify({
-								"type": dnsRecord.type,
-								"name": dnsRecord.name,
-								"content": currentIp,
-								"ttl": dnsRecord.ttl,
-								"proxied": dnsRecord.proxied
-							}),
-							json: true
-						}, function (error2, response2, body2) {
-							if (error2) {
-								throw error2
-							} else if (response2.statusCode != 200) {
-								throw new Error('setting ip address http ' + response2.statusCode);
-							}
-						});
-					}
+		request.get({
+			url: 'https://api.ipify.org/?format=json',
+			json: true
+		}, function (error1, response1, body1) {
+			if (error1) {
+				throw error1
+			} else if (response1.statusCode != 200) {
+				throw new Error('ip address request http ' + response1.statusCode);
+			} else {
+				console.log(body1);
+				const validIp = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/;
+				if (validIp.test(body1.ip)) {
+					const currentIp = validIp.exec(body1.ip)[0];
+					console.log(currentIp);
+					console.log(dnsRecord.type);
+					request.put({
+						url: 'https://api.cloudflare.com/client/v4/zones/' + process.env.CLOUDFLARE_ZONEIDENTIFIER + '/dns_records/' + dnsRecord.id,
+						headers: {
+							'Content-Type': 'application/json',
+							Authorization: 'Bearer ' + process.env.CLOUDFLARE_APIKEY
+						},
+						body: JSON.stringify({
+							"type": dnsRecord.type,
+							"name": dnsRecord.name,
+							"content": currentIp,
+							"ttl": dnsRecord.ttl,
+							"proxied": dnsRecord.proxied
+						}),
+						json: true
+					}, function (error2, response2, body2) {
+						if (error2) {
+							throw error2
+						} else if (response2.statusCode != 200) {
+							throw new Error('setting ip address http ' + response2.statusCode);
+						}
+					});
 				}
-			});
-		}
+			}
+		});
+	}
+}
+
+function updateCloudflareAAAA(dnsRecord) {
+	if (dnsRecord) {
+		request.get({
+			url: 'https://api6.ipify.org/?format=json',
+			json: true
+		}, function (error1, response1, body1) {
+			if (error1) {
+				throw error1
+			} else if (response1.statusCode != 200) {
+				throw new Error('ip address request http ' + response1.statusCode);
+			} else {
+				console.log(body1);
+				const validIp = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/;
+				if (validIp.test(body1.ip)) {
+					const currentIp = validIp.exec(body1.ip)[0];
+					console.log(currentIp);
+					console.log(dnsRecord.type);
+					request.put({
+						url: 'https://api.cloudflare.com/client/v4/zones/' + process.env.CLOUDFLARE_ZONEIDENTIFIER + '/dns_records/' + dnsRecord.id,
+						headers: {
+							'Content-Type': 'application/json',
+							Authorization: 'Bearer ' + process.env.CLOUDFLARE_APIKEY
+						},
+						body: JSON.stringify({
+							"type": dnsRecord.type,
+							"name": dnsRecord.name,
+							"content": currentIp,
+							"ttl": dnsRecord.ttl,
+							"proxied": dnsRecord.proxied
+						}),
+						json: true
+					}, function (error2, response2, body2) {
+						if (error2) {
+							throw error2
+						} else if (response2.statusCode != 200) {
+							throw new Error('setting ip address http ' + response2.statusCode);
+						}
+					});
+				}
+			}
+		});
 	}
 }
